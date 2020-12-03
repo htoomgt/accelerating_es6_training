3f97114a7516b2d487f5cc98020aaf6f1dbea2dc
HTOO MAUNG THAIT <htoo.mt@blueplanet.com.mm>
2020-12-03 22:16:36 +0630

    symbol basic

--- symbols/symbols_basics.js
+++ symbols/symbols_basics.js
@@ -1,1 +1,11 @@
 //code here
+let symbol = Symbol('debug');
+let anotherSymbol = Symbol('debug');
+console.log(typeof symbol);
+console.log(symbol == anotherSymbol);
+
+let obj = {
+    name : 'max',
+    [symbol] : 22
+};
+console.log(obj[symbol]);
