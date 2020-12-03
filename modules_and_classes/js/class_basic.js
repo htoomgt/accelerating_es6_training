22c4a6f9338f16e7dd70dee6d6ee152b7e19cc42
HTOO MAUNG THAIT <htoo.mt@blueplanet.com.mm>
2020-12-03 21:46:24 +0630

    inheritance

--- modules_and_classes/js/class_basic.js
+++ modules_and_classes/js/class_basic.js
@@ -1,14 +1,33 @@
 class Person{
     constructor(name){
         this.name = name;
+        this.age = 20;
     }
     greet(){
-        console.log('Hello, my name is ' + this.name);
+        console.log('Hello, my name is ' + this.name + ". I'm "+ this.age + " years old.");
     }
 
 }
 
+class Max extends Person {
+    constructor(name, age) {
+        super(name);
+        this.age = age;
+    }
+    greet() {
+        console.log("Hello");
+    }
+
+    greetTwice(){
+        super.greet();
+        super.greet();
+    }
+}
+
 let person = new Person('Maxthon');
+let max = new Max('Max', 27);
 // console.log(person);
 person.greet();
+max.greet();
+max.greetTwice();
 console.log(person.__proto__ === Person.prototype);
