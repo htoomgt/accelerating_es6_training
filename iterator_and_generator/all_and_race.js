7473e154560edfd6f74df0a1241c61a386a9e45d
htoo-mg-thait <htoomaungthait@gmail.com>
2021-01-12 22:44:30 +0630

    all and race

---
+++ iterator_and_generator/all_and_race.js
@@ -0,0 +1,19 @@
+let promise1 = new Promise(function(resolve, reject){
+   setTimeout(function(){
+      resolve('Resolved!');
+   }, 1000);
+});
+
+let promise2 =new Promise(function(resolve, reject){
+    setTimeout(function(){
+        reject('Rejected!!');
+    }, 2000);
+});
+
+Promise.race([promise1, promise2])
+    .then(function(success){
+        console.log(success);
+    })
+    .catch(function(error){
+        console.log(error);
+    });
