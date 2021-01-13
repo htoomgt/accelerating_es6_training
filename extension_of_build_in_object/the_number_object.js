64a8fd5fb96dfcac2edccfb766d88d28cfc7845d
htoo-mg-thait <htoomaungthait@gmail.com>
2021-01-13 23:03:06 +0630

    the number object

---
+++ extension_of_build_in_object/the_number_object.js
@@ -0,0 +1,10 @@
+let number = 10000;
+let number2 = Infinity;
+let number3 = 10;
+let number4 = 10.1;
+
+console.log(Number.isNaN(number));
+console.log(Number.isFinite(number));
+console.log(Number.isFinite(number2));
+console.log(Number.isInteger(number3));
+console.log(Number.isInteger(number4));
